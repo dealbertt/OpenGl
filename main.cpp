de037a5239d60a362e722ef92c2db3424a4474ef
dealbertt <dealbertignacio@gmail.com>
2024-05-24 20:26:01 +0200

    Pal PC

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+    "}\0";
+
 //We can also call a function that updates the viewport if the windows is resized using:
 void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
 
@@ -35,35 +50,21 @@ int main(){
     glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
 
 
-    //We can now create the RENDER LOOP
-    while(!glfwWindowShouldClose(window))//An iteration of this loop is what is considered a frame :)
-    {
-        //input
-        processInput(window);
+    
+//VERTEX SHADER
+//-------------------------------------------------------------------------------------------------
 
-
-        //rendering...
-        glClearColor(0.2f,0.3f,0.3f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-
-
-        //check and call events and swap the buffers
-        glfwSwapBuffers(window);//Thos swaps colo buffers
-        glfwPollEvents();//This function checks if there is any event triggered
-        
-    }
-    //To terminate and free all glfw resources we can use:
-    glfwTerminate();
-
-    float vertices[] = {//Because we render in 2d, each vertex has a z coordinate of 0.0
-    //This coordinate are known as Normalized Device Coordinates which is a small place where the x, y and z
-    //values vary from -1.0 to 1.0. Everything outside this range is discarded
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    }; //This are then transformed into screen-space coordinates via teh viewport transform (using the glViewport data)
+float vertices[] = {
+0.5f, 0.5f, 0.0f, // top right
+0.5f, -0.5f, 0.0f, // bottom right
+-0.5f, -0.5f, 0.0f, // bottom left
+-0.5f, 0.5f, 0.0f // top left
+}; //This are then transformed into screen-space coordinates via teh viewport transform (using the glViewport data)
    
+    unsigned int indices[] = {
+        0,1,3,
+        1,2,3
+    };
 
     unsigned int VBO;//We declare our vertex buffer object
 
@@ -74,7 +75,115 @@ int main(){
 
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);//This function copies user-defined data into the bound buffer
 
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);//We pass the type of shader as an argument
 
+    //This function takes the shader object that is gonna be compiled, then the number of strings as source code
+    //Then the source code itself/
+    glShaderSource(vertexShader,1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //To check if the compilation was succesful we can do something like this:
+    int success;
+    char infoLogs[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader,512,NULL,infoLogs);
+        std::cout<<"ERROR::SHADER::VERTEX::COMPILATION:FAILED\n" << infoLogs << std::endl;
+
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    //FRAGMENT SHADER this is used to represent colors
+    //--------------------------------------------------------------------------------------
+    
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+
+    //--------------------------------------------------------------------------------------
+
+    //SHADER PROGRAM this is used to created the final linked version of multiple shaders combined
+    //--------------------------------------------------------------------------------------
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();//This return an ID for the program
+    //We need to attach out shaders into the program:
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    
+
+
+    //We are almost there, we now need to tell OpenGl how to interpret the vertex data before rendering
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+
+    
+    glGetProgramiv(shaderProgram,GL_LINK_STATUS,&success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shaderProgram,512,NULL,infoLogs);
+        std::cout<<"ERROR::SHADER::PROGRAM::COMPILATION:FAILED\n" << infoLogs << std::endl;
+    }
+    glUseProgram(shaderProgram);
+    
+    //We can delete the shader objects once weve linked them into the program object
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    //--------------------------------------------------------------------------------------
+
+    unsigned int VAO;
+    glGenVertexArrays(1,&VAO);
+    
+    unsigned int EBO;
+    glGenBuffers(1,&EBO);
+
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+ 
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+//
+   glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+    
+    //We can now create the RENDER LOOP    
+    while(!glfwWindowShouldClose(window))//An iteration of this loop is what is considered a frame :)
+    {
+        //input
+        processInput(window);
+
+
+        //rendering...
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        glBindVertexArray(0);
+        //glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+
+        //check and call events and swap the buffers
+        glfwSwapBuffers(window);//Thos swaps colo buffers
+        glfwPollEvents();//This function checks if there is any event triggered
+        
+    }
+    //To terminate and free all glfw resources we can use:
+    glfwTerminate();
     return 0;
 }
 
